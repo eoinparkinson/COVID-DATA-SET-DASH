d02cf3214722ad90d3fba3d5edbebe96b5802b71
eoinparkinson <eoinparkinson@users.noreply.github.com>
2020-10-05 14:21:33 +0100

    updates to fix the map

--- app.py
+++ app.py
@@ -19,46 +19,48 @@ colors = {
 df = pd.read_csv("https://raw.githubusercontent.com/eoinparkinson/covid-19-data-raw/master/covid-stats.csv", index_col=0)
 print(df)
 
-#calculate the mean
-#df_mean = df[["CountyName","ConfirmedCovidCases"]].mean()
-
 #opening mapbox token
-token = "pk.eyJ1IjoiZW9pbnBhcmtpbnNvbiIsImEiOiJja2Zzb213MHAwanU0MnFwZGthZjZ1cHljIn0.qX9tz-O9KSxxws35X6LY4Q" # you will need your own token
+token = "pk.eyJ1IjoiZW9pbnBhcmtpbnNvbiIsImEiOiJja2Zzb213MHAwanU0MnFwZGthZjZ1cHljIn0.qX9tz-O9KSxxws35X6LY4Q"
 
-#color_continuous_scale=px.colors.cyclical.IceFire,
+# setting up map_graph
+fig = px.scatter_mapbox(df, lat="Lat", lon="Long", color="CountyName", size="ConfirmedCovidCases",
+                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=40, zoom=5.5)
 
-#fig = px.scatter_mapbox(df, lat="Lat", lon="Long", color="CountyName", size="ConfirmedCovidCases", color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10, mapbox_style="carto-positron") #,barmode="group"
-
-fig = px.scatter_mapbox(df, lat="Lat", lon="Long", color="CountyName", size="ConfirmedCovidCases", size_max=40, zoom=5.5)
 
 #updating map mapbox_style
-fig.update_layout(mapbox_style="dark", mapbox_accesstoken=token)
+fig.update_layout(mapbox_style="light", mapbox_accesstoken=token)
 
+#init layout at /
 app.layout = html.Div(style={
-    "backgroundColor": colors["background"]
-    }, children=[
+    #setting style of header div
+    "backgroundColor": colors["background"],
+    "display": "flex",
+    "flex-direction": "column",
+    },
+    # visual header div & title
+    children=[
     html.H1(children="Ireland Covid-19 Stats",
     style={
         "textAlign": "center",
         "color": colors["text"]
     }),
 
-
+    #main content-div (not the header)
     html.Div(children='''
         by Eoin P.
     ''',
-    style={
+    style={    # style
     "textAlign": "center",
-    "color": colors["sub-text"]
+    "color": colors["sub-text"],
     }),
 
 # implementing the graph
-    html.Div(style={"height":"fill"},children=
+    html.Div(style={"height":"cover"},children=
         dcc.Graph(
             id='example-graph',
             figure=fig,
             style={
-            "height": "80%"
+            "height": "100%"
             })
         )
 ])
