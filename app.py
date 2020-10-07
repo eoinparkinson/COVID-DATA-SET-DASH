40aa37dc5512af8968f9c69eb9c777bc95d705d7
unknown <EoinParkinson2015@DOMAIN.local>
2020-10-07 09:51:11 +0100

    further progress

--- app.py
+++ app.py
@@ -3,10 +3,25 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
+#import numpy as np
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+#columns to remove
+to_drop = [
+            "OBJECTID",
+            "ORIGID",
+            "IGEasting",
+            "IGNorthing",
+            "UGI",
+            "ConfirmedCovidDeaths",
+            "ConfirmedCovidRecovered",
+            "Shape_Area",
+            "Shape_Length"
+        ]
+
 
 # defining some colours
 colors = {
@@ -17,25 +32,29 @@ colors = {
 
 #reading and ingesting datafile
 df = pd.read_csv("https://raw.githubusercontent.com/eoinparkinson/covid-19-data-raw/master/covid-stats.csv", index_col=0)
-#print(df)
 
+#print(df)
+#df.head()
 # cleaning df
-df_clean = df.pivot_table(values="ConfirmedCovidCases", index="CountyName", aggfunc="mean", margins=True)
+#df_clean = df.pivot_table(values="ConfirmedCovidCases", index="CountyName", aggfunc="mean", margins=True)
 #df_clean = df_clean.groupby(["CountyName", "ConfirmedCovidCases"]).mean()
-print(df_clean)
+#print(df_clean)
+
+
+
 
 #opening mapbox token
 token = "pk.eyJ1IjoiZW9pbnBhcmtpbnNvbiIsImEiOiJja2Zzb213MHAwanU0MnFwZGthZjZ1cHljIn0.qX9tz-O9KSxxws35X6LY4Q"
 
 # setting up map_graph
-#fig = px.scatter_mapbox(df_clean, lat="Lat", lon="Long", color="CountyName", size="ConfirmedCovidCases",
-                  #color_continuous_scale=px.colors.cyclical.IceFire, size_max=40, zoom=5.5)
+fig = px.scatter_mapbox(df.groupby(df.index()).mean(), lat="Lat", lon="Long", color="CountyName", size="ConfirmedCovidCases",
+                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=40, zoom=5.5)
 
-fig = px.bar(df_clean, x="CountyName", y="ConfirmedCovidCases", color="CountyName", barmode="group")
+#fig = px.bar(df_clean, x="CountyName", y="ConfirmedCovidCases", color="CountyName", barmode="group")
 
 
 #updating map mapbox_style
-#fig.update_layout(mapbox_style="light", mapbox_accesstoken=token)
+fig.update_layout(mapbox_style="light", mapbox_accesstoken=token)
 
 #init layout at
 app.layout = html.Div(style={
