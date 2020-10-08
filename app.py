227a0679e8fe20fa535f5d88015bd5db43e397c6
unknown <EoinParkinson2015@DOMAIN.local>
2020-10-08 12:05:33 +0100

    Fully cleaned data, properly working map graph

--- app.py
+++ app.py
@@ -3,27 +3,17 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
-#import numpy as np
 
+#external stylesheet for plotly dash
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+#opening mapbox token
+token = "pk.eyJ1IjoiZW9pbnBhcmtpbnNvbiIsImEiOiJja2Zzb213MHAwanU0MnFwZGthZjZ1cHljIn0.qX9tz-O9KSxxws35X6LY4Q"
+
+# initialising/defining the flask/dash app
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-#columns to remove
-to_drop = [
-            "OBJECTID",
-            "ORIGID",
-            "IGEasting",
-            "IGNorthing",
-            "UGI",
-            "ConfirmedCovidDeaths",
-            "ConfirmedCovidRecovered",
-            "Shape_Area",
-            "Shape_Length"
-        ]
-
-
-# defining some colours
+# defining some proppa shwanky colours
 colors = {
     "background": "#EE4266",
     "text": "#FFD23F",
@@ -33,44 +23,24 @@ colors = {
 #reading and ingesting datafile
 df = pd.read_csv("https://raw.githubusercontent.com/eoinparkinson/covid-19-data-raw/master/covid-stats.csv", index_col=0)
 
-
-# cleaning df -----------------
-
-#stuff to drop
-df_clean = df.groupby("CountyName", as_index=False).agg({"ConfirmedCovidCases": "mean"})
+# i cleaning df
+df_clean = df.groupby("CountyName", as_index=False).agg({"ConfirmedCovidCases": "mean", "Lat": "mean", "Long": "mean"})
 print(df_clean)
 
-print("\n\n")
-
+# i description of dataframe
 print(df_clean.describe())
 
-#df_clean = df.pivot_table(values=["CountyName","ConfirmedCovidCases","Lat","Long"], index="ORIGID",aggfunc="mean", margins=True)
 
-
-#df_clean = df_clean.groupby(["CountyName", "ConfirmedCovidCases"]).mean()
-#print(df_clean)
-
-#    .groupby(df.index()).mean()
-
-#df_clean = df.groupby(level=0).mean()
-
-
-#opening mapbox token
-token = "pk.eyJ1IjoiZW9pbnBhcmtpbnNvbiIsImEiOiJja2Zzb213MHAwanU0MnFwZGthZjZ1cHljIn0.qX9tz-O9KSxxws35X6LY4Q"
-
-# setting up map_graph
-#fig = px.scatter_mapbox(df, lat="Lat", lon="Long", color="CountyName", size="ConfirmedCovidCases", color_continuous_scale=px.colors.cyclical.IceFire, size_max=40, zoom=5.5)
-
-fig = px.bar(df_clean, x="CountyName", y="ConfirmedCovidCases", color="CountyName", barmode="group")
-
+# setting up map_fig
+map_fig = px.scatter_mapbox(df_clean.round({"ConfirmedCovidCases":0}), lat="Lat", lon="Long", color="CountyName", size="ConfirmedCovidCases", color_continuous_scale=px.colors.cyclical.IceFire, size_max=40, zoom=5.5)
 
 #updating map mapbox_style
-#fig.update_layout(mapbox_style="open-street-map", mapbox_accesstoken=token)
+map_fig.update_layout(mapbox_style="light", mapbox_accesstoken=token)
 
 #init layout at
 app.layout = html.Div(style={
     #setting style of header div
-    "backgroundColor": colors["background"],
+    "backgroundColor": "#ffffff",
     "display": "flex",
     "flex-direction": "column",
     },
@@ -84,9 +54,10 @@ app.layout = html.Div(style={
 
     #main content-div (not the header)
     html.Div(children='''
-        by Eoin P.
+        Current Graph: Average Map Graph
     ''',
     style={    # style
+    "backgroundColor": colors["background"],
     "textAlign": "center",
     "color": colors["sub-text"],
     }),
@@ -95,7 +66,7 @@ app.layout = html.Div(style={
     html.Div(style={"height":"cover"},children=
         dcc.Graph(
             id='example-graph',
-            figure=fig,
+            figure=map_fig,
             style={
             "height": "800px"
             })
